e45b390571efbca011a94f8bf37f901a19dcf154
Luis Sousa <luisalsousa@outlook.com>
2022-11-15 22:36:47 -0300

    commitandoNovo

---
+++ PrimeiroPrograma/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        System.out.println("atalho = sout => System.out.println");
+
+    }
+}
---
+++ first/src/app/Progran.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Progran {
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!!!");
+
+
+
+    }
+}
