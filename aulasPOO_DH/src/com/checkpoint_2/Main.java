4f6f3f2918493975aa34e2a97a8b059467dd687e
Luis Sousa <luisalsousa@outlook.com>
2022-11-16 22:03:08 -0300

    commitandoNovo2

---
+++ aulasPOO_DH/src/com/checkpoint_2/Main.java
@@ -0,0 +1,15 @@
+package com.checkpoint_2;
+
+public class Main {
+    /**
+    Floricultura
+    Enunciado:
+
+    Uma floricultura da grande BH deseja incorporar um novo sistema de pagamento para melhorar a experiência dos novos clientes.
+    O sistema do DhFlowerPay funciona através da interação do usuário pelo smartphone. Ao iniciar a aplicação, o sistema pedirá que o usuário da floricultura faça seu cadastro.
+    Para o cadastro no sistema, o cliente precisará fornecer seu nome, o sobrenome, o e-mail e o documento do identidade. Além disso, como a floricultura faz entrega poderá também fornecer o endereço para entrega.
+    Após fornecer essas informações a aplicação irá pedir que o cliente confirme se os dados estão corretos. Caso a resposta seja positiva, ele poderá abrir a conta do cliente.
+
+
+     **/
+}
